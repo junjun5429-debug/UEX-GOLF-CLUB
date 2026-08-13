388a3e11170a1da9888d46399550d2c0095f8dcd
junpeiy5429 <junpeiy@microsoft.com>
2026-08-13 17:56:37 +0900

    Compact member editing on iPhone

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const CACHE_NAME = 'uex-golf-club-v13';
-const ASSETS = ['./', './index.html', './styles.css?v=13', './app.js?v=13', './manifest.webmanifest'];
+const CACHE_NAME = 'uex-golf-club-v14';
+const ASSETS = ['./', './index.html', './styles.css?v=14', './app.js?v=14', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
