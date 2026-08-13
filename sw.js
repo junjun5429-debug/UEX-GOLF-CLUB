2f25f3509ae4d41859822e9d0e6a0ce72dc3c435
junpeiy5429 <junpeiy@microsoft.com>
2026-08-13 17:44:59 +0900

    Version static assets for reliable updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const CACHE_NAME = 'uex-golf-club-v11';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+const CACHE_NAME = 'uex-golf-club-v12';
+const ASSETS = ['./', './index.html', './styles.css?v=12', './app.js?v=12', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
