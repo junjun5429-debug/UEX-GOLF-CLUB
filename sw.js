76ece09084791969542fcf7497ef966867bf9e78
junpeiy5429 <junpeiy@microsoft.com>
2026-08-13 18:01:15 +0900

    Further compact member dialog on iPhone

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const CACHE_NAME = 'uex-golf-club-v14';
-const ASSETS = ['./', './index.html', './styles.css?v=14', './app.js?v=14', './manifest.webmanifest'];
+const CACHE_NAME = 'uex-golf-club-v15';
+const ASSETS = ['./', './index.html', './styles.css?v=15', './app.js?v=15', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
